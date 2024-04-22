ae75bdc60e3cb44ef1d8d00b6e552b2557b092f7
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-04-22 15:50:50 +0700

    masukkan void input

--- 0098_IntoOOP/0098_IntoOOP.cpp
+++ 0098_IntoOOP/0098_IntoOOP.cpp
@@ -62,4 +62,20 @@ using namespace std;
 
 class bangunDatar {
 
+    //akses modifier
+    private:
+        float panjang, Lebar;
+    public:
+        float luas;
+
+        void input() { // methode input persegi panjang
+            cout << "Masukkan Panjangnya = ";
+            cin >> panjang;
+            cout << "Masukkan Lebarnya = ";
+            cin >> Lebar;
+        }
+
+        float hitungLuas() {
+            re
+        }
 };
