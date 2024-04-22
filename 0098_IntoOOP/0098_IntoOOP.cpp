dc6fa39a5c1ec2ef781501e847cea964fe8151e2
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-04-22 15:32:55 +0700

    masukkan string kode Mk

--- 0098_IntoOOP/0098_IntoOOP.cpp
+++ 0098_IntoOOP/0098_IntoOOP.cpp
@@ -17,5 +17,21 @@ class Mahasiswa {
 };
 
 class Matakuliah {
-    
+    private:
+        string kodeMk;
+        string namaMk;
+        int sks;
+    public :
+        void input() {
+            cout << "Kode MK: ";
+            cin >> kodeMk;
+            cout << "Nama MK: ";
+            cin >> namaMk;
+            cout << "SKS: ";
+            cin >> sks;
+        }
+
+
+
+
 };
